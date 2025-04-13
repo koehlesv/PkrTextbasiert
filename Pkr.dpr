2796e04997f838eaed75e3fc37777b45218ee1a3
koehlesv <129201373+koehlesv@users.noreply.github.com>
2025-04-13 20:45:36 +0200

    Fastmm4 nur im Debug-Modus.

--- Pkr.dpr
+++ Pkr.dpr
@@ -1,7 +1,9 @@
 program Pkr;
 
 uses
+  {$IFDEF DEBUG}
   FastMM4,
+  {$ENDIF}
   Vcl.Forms,
   MainFrm in 'MainFrm.pas' {frmMain},
   KartenAnzeigenFrm in 'KartenAnzeigenFrm.pas' {frmKartenAnzeigen},
